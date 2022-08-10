38dd1fc7aa58e71961406f3218a870a25fda5023
Adam Lloyd <1369569+Alloyd21@users.noreply.github.com>
2022-08-10 19:34:58 +1000

    Create Upload-JSON2DynamoDB.py

---
+++ Upload-JSON2DynamoDB.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import json
+from msilib.schema import File
+import boto3
+from pprint import pprint
+
+AWS_ACCESS = ""
+AWS_SECRET = ""
+AWS_REGION = ""
+TABLE_NAME = ""
+
+f = open('file.json')
+distros_dict = json.load(f)
+
+items = []
+for row in distros_dict:
+    pprint((row))
+
+
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name=AWS_REGION)
+
+table = dynamodb.Table(TABLE_NAME)
+mylist = distros_dict
+for mydict in mylist:
+    boto3.resource('dynamodb')
+    pprint(mydict)
+    response = table.put_item(Item=mydict)
